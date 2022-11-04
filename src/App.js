f50f5d9dbaef0419e63444f44ece5b4715e4ac4d
Haeryun88 <97426995+Haeryun88@users.noreply.github.com>
2022-11-04 17:32:21 +0900

    2 files modified, 1 added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import D20221103 from './Component/D20221103.js';
+import D20221104 from './Component/D20221104.js';
 
 export default function App() {
   return (
     <div>
-      <h1>엥</h1>
+      <h1>업무일지</h1>
       <D20221103 />
+      <D20221104 />
     </div>
   );
 }
