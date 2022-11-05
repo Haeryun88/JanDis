11eacd1456947e43ce3305920c6fb1c27ba02198
Haeryun88 <97426995+Haeryun88@users.noreply.github.com>
2022-11-05 17:07:55 +0900

    2022년 11월 5일 작성

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import D20221103 from './Component/D20221103.js';
 import D20221104 from './Component/D20221104.js';
+import D20221105 from './Component/D20221105.js';
 
 export default function App() {
   return (
@@ -8,6 +9,7 @@ export default function App() {
       <h1>업무일지</h1>
       <D20221103 />
       <D20221104 />
+      <D20221105 />
     </div>
   );
 }
