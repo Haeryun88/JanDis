4d5c813958246a921106de5adb67eaf1261d1c28
Haeryun88 <97426995+Haeryun88@users.noreply.github.com>
2022-11-12 14:42:42 +0900

    2 files modified, 1 added

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import D20221104 from './Component/D20221104.js';
 import D20221105 from './Component/D20221105.js';
 import D20221107 from './Component/D20221107.js';
 import D20221108 from './Component/D20221108.js';
+import D20221112 from './Component/D20221112.js';
 
 export default function App() {
   return (
@@ -15,6 +16,7 @@ export default function App() {
       <D20221105 />
       <D20221107 />
       <D20221108 />
+      <D20221112 />
     </div>
   );
 }
